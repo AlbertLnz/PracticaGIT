0265a73e056678f1df5046f92558c5100ba00df0
AlbertLnz <120119395+AlbertLnz@users.noreply.github.com>
2023-03-01 13:19:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ do{
 var_dump($autoList);
 
 //canvi per fer el 3r repositori
+//canvi realitzat desde el editor de GITHUB
 // // function pedirDatos($dato){
 // //   return readlines("Dime el/la ".$dato.": ");
 // // }
